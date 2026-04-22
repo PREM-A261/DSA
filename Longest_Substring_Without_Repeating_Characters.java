f0f58c374f6ae6475f4c903fb20165ea6c660b72
PREM PAWAR <145797233+PREM-A261@users.noreply.github.com>
2026-04-22 23:57:20 +0530

    Add files via upload

---
+++ Longest_Substring_Without_Repeating_Characters.java
@@ -0,0 +1,26 @@
+package DAY3;
+
+import java.util.HashSet;
+
+public class Longest_Substring_Without_Repeating_Characters {
+    public static void main(String[] args) {
+        String s = "abcdabcbb";
+        HashSet<Character> set = new HashSet<>();
+        int maxLength = 0;
+        int left = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            while (set.contains(s.charAt(right))) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+
+            int window = right - left + 1;
+            set.add(s.charAt(right));
+            maxLength = Math.max(maxLength, window);
+        }
+
+        System.out.println(maxLength);
+
+    }
+}
