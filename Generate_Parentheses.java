46b7ce170813beef325e630cf8e2f8def86ea0fd
PREM PAWAR <145797233+PREM-A261@users.noreply.github.com>
2026-06-25 16:46:01 +0530

    Add files via upload

---
+++ Generate_Parentheses.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Generate_Parentheses {
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<String>();
+        recurse(res, 0, 0, "", n);
+        return res;
+    }
+    
+    public void recurse(List<String> res, int left, int right, String s, int n) {
+        if (s.length() == n * 2) {
+            res.add(s);
+            return;
+        }
+        if (left < n) {
+            recurse(res, left + 1, right, s + "(", n);
+        }
+        if (right < left) {
+            recurse(res, left, right + 1, s + ")", n);
+        }
+    }
+
+    public static void main(String[] args) {
+        Generate_Parentheses obj = new Generate_Parentheses();
+        int n = 3;
+        List<String> result = obj.generateParenthesis(n);
+        System.out.println(result); 
+    }
+}
